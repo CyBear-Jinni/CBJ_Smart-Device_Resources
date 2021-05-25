40f801a9948d8027f0fdd162399c607324a8470f
Guy Luz <guyluz11@gmail.com>
2021-05-25 16:27:45 +0300

    Update setup_new_smart_device.sh

--- devices_resources/raspberry_pi/setup_new_smart_device.sh
+++ devices_resources/raspberry_pi/setup_new_smart_device.sh
@@ -3,8 +3,8 @@
 # Add the directory and configuration file:
 mkdir -p /etc/cbjinni/
 
-echo 'Light, ' >/etc/cbjinni/deviceConfigs.txt
-#echo 'Blinds, ' > /etc/cbjinni/deviceConfigs.txt  # 'Light,' and 'Light, Light, ' and 'Blinds,' can be used
+echo 'light, ' >/etc/cbjinni/deviceConfigs.txt
+#echo 'blinds, ' > /etc/cbjinni/deviceConfigs.txt  # 'light,' and 'light, light, ' and 'blinds,' can be used
 
 # Connect device to the wifi
 nmcli dev wifi connect wifiSsid password wifiPassword
